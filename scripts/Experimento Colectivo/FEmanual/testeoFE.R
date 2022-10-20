8552735babebf05198a1b5c4ab7fe30016172a11
gscoccola <79168517+gscoccola@users.noreply.github.com>
2022-10-20 15:30:59 -0300

    Add files via upload

---
+++ scripts/Experimento Colectivo/FEmanual/testeoFE.R
@@ -0,0 +1,251 @@
+#Modificado para correr en la computadora local
+
+# son varios archivos, subirlos INTELIGENTEMENTE a Kaggle
+
+#limpio la memoria
+#rm( list=ls() )  #remove all objects
+#gc()             #garbage collection
+
+require("data.table")
+require("lightgbm")
+require("ggplot2")
+
+#defino los parametros de la corrida, en una lista, la variable global  PARAM
+#  muy pronto esto se leera desde un archivo formato .yaml
+PARAM <- list()
+PARAM$experimento  <- "FEmanuales"
+
+PARAM$input$dataset       <- "./competencia2_2022.csv.gz"
+PARAM$input$training      <- c( 202101 )
+PARAM$input$future        <- c( 202103 )
+
+#Estan puestos los de la BO, comentados los de la BO sin varaibles adicionales
+PARAM$finalmodel$max_bin           <-     31
+PARAM$finalmodel$learning_rate     <-      0.0289229271953041   #0.0437907293266036
+PARAM$finalmodel$num_iterations    <-    235  #164
+PARAM$finalmodel$num_leaves        <-   891  #1024
+PARAM$finalmodel$min_data_in_leaf  <-   840  #1239
+PARAM$finalmodel$feature_fraction  <-      0.707960734579829  #0.324462828627375
+PARAM$finalmodel$semilla           <- 448139
+#100153, 671017, 202061, 348431, 448139
+PARAM$finalmodel$lambda_l1           <-     0
+PARAM$finalmodel$lambda_l2           <-     0
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Aqui empieza el programa
+
+
+#cargo el dataset donde voy a entrenar
+dataset  <- fread(PARAM$input$dataset, stringsAsFactors= TRUE)
+
+#Único feature Engineering
+
+#creo un ctr_quarter que tenga en cuenta cuando los clientes hace 3 menos meses que estan
+dataset[  , ctrx_quarter_normalizado := ctrx_quarter ]
+dataset[ cliente_antiguedad==1 , ctrx_quarter_normalizado := ctrx_quarter * 5 ]
+dataset[ cliente_antiguedad==2 , ctrx_quarter_normalizado := ctrx_quarter * 2 ]
+dataset[ cliente_antiguedad==3 , ctrx_quarter_normalizado := ctrx_quarter * 1.2 ]
+
+#variable extraida de una tesis de maestria de Irlanda
+dataset[  , mpayroll_sobre_edad  := mpayroll / cliente_edad ]
+
+#se crean los nuevos campos para MasterCard  y Visa, teniendo en cuenta los NA's
+#varias formas de combinar Visa_status y Master_status
+dataset[ , mv_status01       := pmax( Master_status,  Visa_status, na.rm = TRUE) ]
+dataset[ , mv_status02       := Master_status +  Visa_status ]
+dataset[ , mv_status03       := pmax( ifelse( is.na(Master_status), 10, Master_status) , ifelse( is.na(Visa_status), 10, Visa_status) ) ]
+dataset[ , mv_status04       := ifelse( is.na(Master_status), 10, Master_status)  +  ifelse( is.na(Visa_status), 10, Visa_status)  ]
+dataset[ , mv_status05       := ifelse( is.na(Master_status), 10, Master_status)  +  100*ifelse( is.na(Visa_status), 10, Visa_status)  ]
+
+dataset[ , mv_status06       := ifelse( is.na(Visa_status), 
+                                        ifelse( is.na(Master_status), 10, Master_status), 
+                                        Visa_status)  ]
+
+dataset[ , mv_status07       := ifelse( is.na(Master_status), 
+                                        ifelse( is.na(Visa_status), 10, Visa_status), 
+                                        Master_status)  ]
+
+
+#combino MasterCard y Visa
+dataset[ , mv_mfinanciacion_limite := rowSums( cbind( Master_mfinanciacion_limite,  Visa_mfinanciacion_limite) , na.rm=TRUE ) ]
+
+dataset[ , mv_Fvencimiento         := pmin( Master_Fvencimiento, Visa_Fvencimiento, na.rm = TRUE) ]
+dataset[ , mv_Finiciomora          := pmin( Master_Finiciomora, Visa_Finiciomora, na.rm = TRUE) ]
+dataset[ , mv_msaldototal          := rowSums( cbind( Master_msaldototal,  Visa_msaldototal) , na.rm=TRUE ) ]
+dataset[ , mv_msaldopesos          := rowSums( cbind( Master_msaldopesos,  Visa_msaldopesos) , na.rm=TRUE ) ]
+dataset[ , mv_msaldodolares        := rowSums( cbind( Master_msaldodolares,  Visa_msaldodolares) , na.rm=TRUE ) ]
+dataset[ , mv_mconsumospesos       := rowSums( cbind( Master_mconsumospesos,  Visa_mconsumospesos) , na.rm=TRUE ) ]
+dataset[ , mv_mconsumosdolares     := rowSums( cbind( Master_mconsumosdolares,  Visa_mconsumosdolares) , na.rm=TRUE ) ]
+dataset[ , mv_mlimitecompra        := rowSums( cbind( Master_mlimitecompra,  Visa_mlimitecompra) , na.rm=TRUE ) ]
+dataset[ , mv_madelantopesos       := rowSums( cbind( Master_madelantopesos,  Visa_madelantopesos) , na.rm=TRUE ) ]
+dataset[ , mv_madelantodolares     := rowSums( cbind( Master_madelantodolares,  Visa_madelantodolares) , na.rm=TRUE ) ]
+dataset[ , mv_fultimo_cierre       := pmax( Master_fultimo_cierre, Visa_fultimo_cierre, na.rm = TRUE) ]
+dataset[ , mv_mpagado              := rowSums( cbind( Master_mpagado,  Visa_mpagado) , na.rm=TRUE ) ]
+dataset[ , mv_mpagospesos          := rowSums( cbind( Master_mpagospesos,  Visa_mpagospesos) , na.rm=TRUE ) ]
+dataset[ , mv_mpagosdolares        := rowSums( cbind( Master_mpagosdolares,  Visa_mpagosdolares) , na.rm=TRUE ) ]
+dataset[ , mv_fechaalta            := pmax( Master_fechaalta, Visa_fechaalta, na.rm = TRUE) ]
+dataset[ , mv_mconsumototal        := rowSums( cbind( Master_mconsumototal,  Visa_mconsumototal) , na.rm=TRUE ) ]
+dataset[ , mv_cconsumos            := rowSums( cbind( Master_cconsumos,  Visa_cconsumos) , na.rm=TRUE ) ]
+dataset[ , mv_cadelantosefectivo   := rowSums( cbind( Master_cadelantosefectivo,  Visa_cadelantosefectivo) , na.rm=TRUE ) ]
+dataset[ , mv_mpagominimo          := rowSums( cbind( Master_mpagominimo,  Visa_mpagominimo) , na.rm=TRUE ) ]
+
+#a partir de aqui juego con la suma de Mastercard y Visa
+dataset[ , mvr_Master_mlimitecompra:= Master_mlimitecompra / mv_mlimitecompra ]
+dataset[ , mvr_Visa_mlimitecompra  := Visa_mlimitecompra / mv_mlimitecompra ]
+dataset[ , mvr_msaldototal         := mv_msaldototal / mv_mlimitecompra ]
+dataset[ , mvr_msaldopesos         := mv_msaldopesos / mv_mlimitecompra ]
+dataset[ , mvr_msaldopesos2        := mv_msaldopesos / mv_msaldototal ]
+dataset[ , mvr_msaldodolares       := mv_msaldodolares / mv_mlimitecompra ]
+dataset[ , mvr_msaldodolares2      := mv_msaldodolares / mv_msaldototal ]
+dataset[ , mvr_mconsumospesos      := mv_mconsumospesos / mv_mlimitecompra ]
+dataset[ , mvr_mconsumosdolares    := mv_mconsumosdolares / mv_mlimitecompra ]
+dataset[ , mvr_madelantopesos      := mv_madelantopesos / mv_mlimitecompra ]
+dataset[ , mvr_madelantodolares    := mv_madelantodolares / mv_mlimitecompra ]
+dataset[ , mvr_mpagado             := mv_mpagado / mv_mlimitecompra ]
+dataset[ , mvr_mpagospesos         := mv_mpagospesos / mv_mlimitecompra ]
+dataset[ , mvr_mpagosdolares       := mv_mpagosdolares / mv_mlimitecompra ]
+dataset[ , mvr_mconsumototal       := mv_mconsumototal  / mv_mlimitecompra ]
+dataset[ , mvr_mpagominimo         := mv_mpagominimo  / mv_mlimitecompra ]
+
+#Aqui debe usted agregar sus propias nuevas variables
+
+#valvula de seguridad para evitar valores infinitos
+#paso los infinitos a NULOS
+infinitos      <- lapply(names(dataset),function(.name) dataset[ , sum(is.infinite(get(.name)))])
+infinitos_qty  <- sum( unlist( infinitos) )
+if( infinitos_qty > 0 )
+{
+  cat( "ATENCION, hay", infinitos_qty, "valores infinitos en tu dataset. Seran pasados a NA\n" )
+  dataset[mapply(is.infinite, dataset)] <- NA
+}
+
+
+#valvula de seguridad para evitar valores NaN  que es 0/0
+#paso los NaN a 0 , decision polemica si las hay
+#se invita a asignar un valor razonable segun la semantica del campo creado
+nans      <- lapply(names(dataset),function(.name) dataset[ , sum(is.nan(get(.name)))])
+nans_qty  <- sum( unlist( nans) )
+if( nans_qty > 0 )
+{
+  cat( "ATENCION, hay", nans_qty, "valores NaN 0/0 en tu dataset. Seran pasados arbitrariamente a 0\n" )
+  cat( "Si no te gusta la decision, modifica a gusto el programa!\n\n")
+  dataset[mapply(is.nan, dataset)] <- 0
+}
+
+#--------------------------------------
+
+#paso la clase a binaria que tome valores {0,1}  enteros
+#set trabaja con la clase  POS = { BAJA+1, BAJA+2 } 
+#esta estrategia es MUY importante
+dataset[ , clase01 := ifelse( clase_ternaria %in%  c("BAJA+2","BAJA+1"), 1L, 0L) ]
+
+#--------------------------------------
+
+#los campos que se van a utilizar
+campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01") )
+
+#--------------------------------------
+
+
+
+#establezco donde entreno
+dataset[ , train  := 0L ]
+dataset[ foto_mes %in% PARAM$input$training, train  := 1L ]
+
+#--------------------------------------
+#creo las carpetas donde van los resultados
+#creo la carpeta donde va el experimento
+# HT  representa  Hiperparameter Tuning
+dir.create( "./exp/",  showWarnings = FALSE ) 
+dir.create( paste0("./exp/", PARAM$experimento, "/" ), showWarnings = FALSE )
+setwd( "C:\\Users\\PC\\Desktop\\DMEyF\\Experimento Colectivo" )   #Establezco el Working Directory DEL EXPERIMENTO
+
+
+
+#dejo los datos en el formato que necesita LightGBM
+dtrain  <- lgb.Dataset( data= data.matrix(  dataset[ train==1L, campos_buenos, with=FALSE]),
+                        label= dataset[ train==1L, clase01] )
+
+#genero el modelo
+#estos hiperparametros  salieron de una laaarga Optmizacion Bayesiana
+modelo  <- lgb.train( data= dtrain,
+                      param= list( objective=          "binary",
+                                   max_bin=            PARAM$finalmodel$max_bin,
+                                   learning_rate=      PARAM$finalmodel$learning_rate,
+                                   num_iterations=     PARAM$finalmodel$num_iterations,
+                                   num_leaves=         PARAM$finalmodel$num_leaves,
+                                   min_data_in_leaf=   PARAM$finalmodel$min_data_in_leaf,
+                                   feature_fraction=   PARAM$finalmodel$feature_fraction,
+                                   seed=               PARAM$finalmodel$semilla
+                      )
+)
+
+#--------------------------------------
+#ahora imprimo la importancia de variables
+tb_importancia  <-  as.data.table( lgb.importance(modelo) ) 
+archivo_importancia  <- "impo.txt"
+
+fwrite( tb_importancia, 
+        file= archivo_importancia, 
+        sep= "\t" )
+
+#--------------------------------------
+
+
+#aplico el modelo a los datos sin clase
+dapply  <- dataset[ foto_mes== PARAM$input$future ]
+
+#aplico el modelo a los datos nuevos
+prediccion  <- predict( modelo, 
+                        data.matrix( dapply[, campos_buenos, with=FALSE ]) ) 
+
+
+
+#genero la tabla de entrega
+tb_entrega  <-  dapply[ , list( numero_de_cliente, foto_mes, clase_ternaria ) ]
+tb_entrega[  , prob := prediccion ]
+
+#grabo las probabilidad del modelo
+fwrite( tb_entrega,
+        file= "prediccion.txt",
+        sep= "\t" )
+
+#ordeno por probabilidad descendente
+setorder( tb_entrega, -prob )
+
+ganancias <- list()
+#genero archivos con los  "envios" mejores
+#deben subirse "inteligentemente" a Kaggle para no malgastar submits
+cortes <- seq( 5000, 15000, by=250 )
+for( envios  in  cortes )
+{
+  tb_entrega[  , Predicted := 0L ]
+  tb_entrega[ 1:envios, Predicted := 1L ]
+  ganancia <- ((-2000)*length(tb_entrega[Predicted==1 & clase_ternaria!="BAJA+2", Predicted])
+               + 78000*length(tb_entrega[Predicted==1 & clase_ternaria=="BAJA+2", Predicted]))/1000000
+  
+  ganancias <- append(ganancias, ganancia)
+  
+  
+  
+}
+
+envios1=as.list(cortes)
+
+
+resultados <- data.frame(envios=unlist(envios1), ganancias=unlist(ganancias))
+
+
+intervalo_max <- resultados[as.integer(which.max(resultados$ganancias)-5):as.integer(which.max(resultados$ganancias)+5),]
+
+ganancia_aprox <- round(mean(intervalo_max$ganancias), 4)
+
+
+
+#mean(c(25.5091, 24.76, 24.9055, 24.9345, 24.7764))
+print(ganancia_aprox)
+r5 <- resultados
+
+
